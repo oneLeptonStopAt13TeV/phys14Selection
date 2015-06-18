616ef1295823b0c6d184ff8f7ff1eaddf5e94cf3
Eric Chabert <Eric.Chabert@cern.ch>
2015-06-18 21:43:38 +0200

    change the return type

--- inputs/hadchi2/hadchi2.py
+++ inputs/hadchi2/hadchi2.py
@@ -1,10 +1,17 @@
 import ctypes
 import os
 from ROOT import TLorentzVector
+from rootpy import log
+
+log["/ROOT.Minuit2"].setLevel(log.ERROR)
+log["/ROOT.Minuit"].setLevel(log.ERROR)
 
 lib = os.path.dirname(os.path.realpath(__file__))+'/libHadChi2.so'
 chi2wrap = ctypes.CDLL(lib)
-Chi2compute_ = chi2wrap.ComputeChi2
+#Chi2compute_ = chi2wrap.ComputeChi2
+Chi2compute_ = chi2wrap.Chi2Interface
+#Chi2compute_ = chi2wrap.calculateChi2
+#Chi2compute_ = chi2wrap.ComputeChi2
 
 # Arguments types
 Chi2compute_.argtypes = (ctypes.c_int, 
@@ -31,5 +38,5 @@ def computeHadChi2 (jets):
 
     
     result = Chi2compute_( ctypes.c_int(nJets), 
-                          jetsDoubleArray(*jetsPt), jetsDoubleArray(*jetsPhi), jetsDoubleArray(*jetsEta), jetsDoubleArray(*jetsEnergy), jetsBoolArray(*jetsBTagged))
-    return long(result)
+                          jetsDoubleArray(*jetsPt), jetsDoubleArray(*jetsEta), jetsDoubleArray(*jetsPhi), jetsDoubleArray(*jetsEnergy), jetsBoolArray(*jetsBTagged))
+    return float(result)
